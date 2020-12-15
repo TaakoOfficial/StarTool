59e3df109d78e2b2d195c0a2dddf892d03c44576
Visen Gaming <visen.gaming@gmail.com>
2020-12-14 20:10:45 -0600

    Updates Main Code.

--- src/main/java/me/toonamisenpai/startool/Main.java
+++ src/main/java/me/toonamisenpai/startool/Main.java
@@ -1,17 +1,75 @@
 package me.toonamisenpai.startool;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin {
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Main extends JavaPlugin implements Listener {
+
+	public List<String> list = new ArrayList<String>();
 
 	@Override
 	public void onEnable() {
-		// Plugin startup logic
-
+		this.getServer().getPluginManager().registerEvents(new StarEvents(this), this);
 	}
 
 	@Override
 	public void onDisable() {
-		// Plugin shutdown logic yes
+
 	}
+
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (label.equalsIgnoreCase("startool")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage(ChatColor.DARK_RED + "You cannot run this command!");
+				return true;
+			}
+			Player player = (Player) sender;
+			if (player.getInventory().firstEmpty() == -1) {
+				Location loc = player.getLocation();
+				World world = player.getWorld();
+
+				world.dropItemNaturally(loc, getItem());
+				player.sendMessage(ChatColor.GOLD + "The Gods of Minecraft dropped a gift near you.");
+				return true;
+			}
+			player.getInventory().addItem(getItem());
+			player.sendMessage(ChatColor.GOLD + "The Gods of Minecraft gave you a gift.");
+			return true;
+		}
+		return false;
+	}
+
+	public ItemStack getItem() {
+
+		ItemStack item = new ItemStack(Material.TRIDENT);
+		ItemMeta meta = item.getItemMeta();
+
+		meta.setDisplayName(ChatColor.DARK_GREEN + "" + ChatColor.BOLD + "Ancient Trident");
+		List<String> lore = new ArrayList<String>();
+		lore.add("");
+		lore.add(ChatColor.translateAlternateColorCodes('&', "&7(Right Click) &a&oSpawn minions"));
+		lore.add(ChatColor.translateAlternateColorCodes('&', "&7(Left Click) &a&oShoot explosives"));
+		meta.setLore(lore);
+
+		meta.addEnchant(Enchantment.LOYALTY, 10, true);
+		meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+		meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+
+		item.setItemMeta(meta);
+
+		return item;
+	}
+
 }
